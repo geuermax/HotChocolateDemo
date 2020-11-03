80f60547b7593828858f1a65c27509d93c41f9c6
Max Geuer <geuermax@gmail.com>
2020-11-03 21:23:35 +0100

    added new middleware and connection manager to keep track of ws and to close those after time

---
+++ HotChocolateDemo/hcWebsockets/MySubscriptionMiddleware.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Threading.Tasks;
+using HotChocolate.AspNetCore.Subscriptions;
+using HotChocolate.Execution;
+using Microsoft.AspNetCore.Http;
+
+namespace HotChocolateDemo.hcWebsockets
+{
+    public class MySubscriptionMiddleware
+    {
+
+        private readonly RequestDelegate _next;
+        private readonly IMessagePipeline _messagePipeline;
+        private readonly SubscriptionMiddlewareOptions _options;
+
+        private MyWebSocketManager socketManager;
+
+        public MySubscriptionMiddleware(
+            RequestDelegate next,
+            IMessagePipeline messagePipeline,
+            SubscriptionMiddlewareOptions options,
+            MyWebSocketManager socketManager)
+        {
+            _next = next
+                ?? throw new ArgumentNullException(nameof(next));
+            _messagePipeline = messagePipeline
+                ?? throw new ArgumentNullException(nameof(messagePipeline));
+            _options = options
+                ?? throw new ArgumentNullException(nameof(options));
+            
+            this.socketManager = socketManager;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            
+            if (context.WebSockets.IsWebSocketRequest)
+            {
+                var wsSession = WebSocketSession.New(context, _messagePipeline);              
+                socketManager.AddSocketSession(wsSession);
+                await wsSession.HandleAsync(context.RequestAborted).ConfigureAwait(false);
+
+            }
+            else
+            {
+                await _next(context).ConfigureAwait(false);
+            }
+        }
+
+        public static bool IsValidPath(HttpContext context, PathString path)
+        {
+            return context.Request.Path.Equals(path,
+                StringComparison.OrdinalIgnoreCase);
+        }
+
+    }
+}
---
+++ HotChocolateDemo/hcWebsockets/MyWebSocketManager.cs
@@ -0,0 +1,67 @@
+﻿using HotChocolate.Server;
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace HotChocolateDemo.hcWebsockets
+{
+    public class MyWebSocketManager
+    {
+
+        private readonly ConcurrentDictionary<string, ISocketSession> _sessions = new ConcurrentDictionary<string, ISocketSession>();
+        private readonly ConcurrentDictionary<string, System.Timers.Timer> _timers = new ConcurrentDictionary<string, System.Timers.Timer>();
+        
+
+        public ConcurrentDictionary<string, ISocketSession> GetAll()
+        {
+            return _sessions;
+        }
+
+        public string GetSocketSessionId(ISocketSession session)
+        {
+            return _sessions.FirstOrDefault(s => s.Value == session).Key;
+        }
+
+        public ISocketSession GetSocketSessionById(string key)
+        {
+            return _sessions.FirstOrDefault(s => s.Key == key).Value;
+        }
+
+        public void AddSocketSession(ISocketSession socketSession)
+        {
+            string id = CreateId();
+            Console.WriteLine("Add socket to list. ID: {0} \tTime: {1}", id, DateTime.Now);
+            _sessions.TryAdd(id, socketSession);
+            
+            var timer = new System.Timers.Timer(15*1000*60);
+            timer.Elapsed += (sender, e) =>
+            {
+                Console.WriteLine("Closing socket. ID: {0} \tTime: {1}", id, DateTime.Now);
+                RemoveSocketSesion(id);
+            };
+            timer.AutoReset = false;
+            _timers.TryAdd(id, timer);
+            timer.Start();            
+        }
+
+        public void RemoveSocketSesion(string id)
+        {            
+            ISocketSession socketSession;
+            System.Timers.Timer timer;
+            _sessions.TryRemove(id, out socketSession);
+            _timers.TryRemove(id, out timer);
+            timer.Stop();
+            socketSession.Dispose();
+        }
+
+        private string CreateId()
+        {
+            return Guid.NewGuid().ToString();
+        }
+
+    }
+}
